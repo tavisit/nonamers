c1773d7d7089a3982b73dbbfc120af1985d728fd
Matei Octavian <mateitavis@gmail.com>
2020-12-05 23:24:21 +0200

    Pages done To do: link the pages

---
+++ js/login.js
@@ -0,0 +1,14 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  function openForm_reg() {
+    document.getElementById("myForm_register").style.display = "block";
+  }
+  
+  function closeForm_reg() {
+    document.getElementById("myForm_register").style.display = "none";
+  }
